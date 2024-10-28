93973ed6dc4c09b0ba6dca4dd72a3da2059d2a77
Vishan Sonagadu <vishansonagadu@gmail.com>
2024-10-27 20:44:18 -0400

    Implement function to extract tokens from Bison's syntax error message

--- src/error/error.cpp
+++ src/error/error.cpp
@@ -7,3 +7,26 @@ void printErrorMessage(int lineNumber, std::string errorType, std::string errorM
 
     std::cout << message << std::endl;
 }
+
+std::vector<std::string> extractExpectedTokens(const std::string& s) {
+    std::vector<std::string> expectedTokens;
+
+    size_t expectingPos = s.find("expecting");
+    if (expectingPos == std::string::npos) return expectedTokens;
+
+    // Get the substring after "expecting "
+    std::string expectedPart = s.substr(expectingPos + 10);  // Skip "expecting "
+
+    // Split by " or " to get each expected token
+    size_t start = 0;
+    size_t end = expectedPart.find(" or ");
+    while (end != std::string::npos) {
+        expectedTokens.push_back(expectedPart.substr(start, end - start));
+        start = end + 4;  // Move past " or "
+        end = expectedPart.find(" or ", start);
+    }
+    // Add the last token after the final " or "
+    expectedTokens.push_back(expectedPart.substr(start));
+
+    return expectedTokens;
+}
--- src/error/error.h
+++ src/error/error.h
@@ -5,6 +5,7 @@
 #include <string>
 
 void printErrorMessage(int lineNumber, std::string errorType, std::string errorMessage);
+std::vector<std::string> extractExpectedTokens(const std::string& s);
 
 
 #endif
